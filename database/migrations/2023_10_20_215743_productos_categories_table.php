a6fdf7ff84f6d0877e645111d215756423d69544
richar2005 <1431574@senati.pe>
2023-10-20 18:53:18 -0500

    TrabajoFinal1

---
+++ database/migrations/2023_10_20_215743_productos_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('Productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->float('precio')->unique();
+            $table->text('descripcion');
+            $table->integer('existencias');
+            $table->text('categoria');
+            $table->text('proveedor');
+           
+         
+            $table->rememberToken();
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('Productos');
+    }
+};
